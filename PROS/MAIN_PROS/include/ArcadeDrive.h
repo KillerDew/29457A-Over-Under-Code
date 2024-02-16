8fcf0700cdb931b33f8954ebed76b428d33fd3df
KillerDew <daniel.dew007@gmail.com>
2024-02-16 17:10:15 +0000

    Working Drivetrain, working cata
    using PROS

---
+++ PROS/MAIN_PROS/include/ArcadeDrive.h
@@ -0,0 +1,3 @@
+#include "main.h"
+void Drive();
+double* getVelocitys(double x, double y, double deadzoneX, double deadzoneY);
--- PROS/MAIN_PROS/src/main.cpp
+++ PROS/MAIN_PROS/src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -24,6 +25,8 @@ void on_center_button() {
  */
 std::shared_ptr<OdomChassisController> chassis;
 std::shared_ptr<AsyncPositionController<double, double>> IntakeController;
+std::shared_ptr<AsyncVelocityController<double, double>> IntakeController_;
+pros::Controller Master (pros::E_CONTROLLER_MASTER);
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "??");
@@ -36,7 +39,18 @@ void initialize() {
 			)
 			.withDimensions(AbstractMotor::gearset::green, {{3.75_in, 12_in}, imev5GreenTPR})
 			.withOdometry({{3.25_in, 12_in}, quadEncoderTPR}, StateMode::FRAME_TRANSFORMATION)
-			.buildOdometry(); 
+			.buildOdometry();
+	IntakeController_ = 
+		AsyncVelControllerBuilder()
+			.withMotor(-14)
+			.withMotor(15)
+			.withMaxVelocity(200)
+			.build();
+	
+
+	
+	
+
 
 }
 
@@ -73,7 +87,10 @@ void competition_initialize() {
 void autonomous() {
 	chassis -> setState({0_in, 0_in, 0_deg});
 	chassis -> setMaxVelocity(50);
-	chassis -> driveToPoint({0_ft, 2_ft})
+	chassis -> driveToPoint({0_ft, 2_ft});
+	IntakeController_ -> setTarget(-200);
+	chassis -> waitUntilSettled();
+	chassis -> driveToPoint({0_ft, 0_ft});
 	
 }
 
@@ -90,6 +107,37 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+pros::Motor Catapult (7);
+pros::MotorGroup motors_left({18, 19, 20});
+pros::MotorGroup motors_right({8, 9, 10});
+pros::MotorGroup IntakeMotors({-14, 15});
+double CataSpeed = 0.4;
 void opcontrol() {
-	autonomous();
+	Catapult.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	Catapult.set_gearing(pros::E_MOTOR_GEARSET_36);
+	while(1) {
+        // Retrieve the necessary joystick values
+        int leftX = -Master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+        int leftY = -Master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        
+        // Move the left side of the robot
+        motors_left.move(leftX + leftY);
+        
+        // Move the right side of the robot 
+        motors_right.move(leftX - leftY);
+
+		if (Master.get_digital(pros::E_CONTROLLER_DIGITAL_X)){
+			Catapult.move_velocity(-100 * CataSpeed);
+		}else{
+			Catapult = 0;
+		}
+		if (Master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+			IntakeMotors.move(-127);
+		}else if (Master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
+			IntakeMotors.move(127);
+		}else
+			IntakeMotors = 0;
+		}
+        pros::delay(20);
+    }
 }
